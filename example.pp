701200fe0e765eee7d1f56c6091912df024d5143
danielfobooss <dani.fortuna97@gmail.com>
2022-03-17 17:10:02 +0000

    Create example.pp

---
+++ example.pp
@@ -0,0 +1,7 @@
+# addresses bug: https://bugs.launchpad.net/keystone/+bug/1472285
+$bind_host = '0.0.0.0'
+$quantum_auth_url = 'http://127.0.0.1:35357/v2.0'
+$str = 'hey'
+$message = sha1($str)
+$postgresql_version = '9.6'
+$cenas = 'hi'
